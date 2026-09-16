235bc470b3919ad13c2833f4b20a3033902fcfbe
Luis Muñoz <luisfelipee2203@gmail.com>
2026-09-15 23:31:42 -0500

    dtos

--- src/main/java/com/deepblue/rescue/dto/request/ChangeRescueStatusRequest.java
+++ src/main/java/com/deepblue/rescue/dto/request/ChangeRescueStatusRequest.java
@@ -1,5 +1,8 @@
 package com.deepblue.rescue.dto.request;
 
-public class ChangeRescueStatusRequest {
-    
-}
+import com.deepblue.rescue.domain.RescueStatus;
+
+public record ChangeRescueStatusRequest( 
+    RescueStatus status
+) {
+} 
